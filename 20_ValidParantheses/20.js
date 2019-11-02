f05062bc76fba7b6206d0f89845b0133d231bc96
spac3nerd <sorin.badila@yahoo.com>
2019-11-01 21:04:38 -0400

    New solution for 20

--- 20_ValidParantheses/20.js
+++ 20_ValidParantheses/20.js
@@ -2,7 +2,8 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+//solution 1 - used a switch statement, I think this can be faster
+var isValid_s1 = function(s) {
 
     function verifyPair(a, b) {
         switch (a) {
@@ -35,6 +36,38 @@ var isValid = function(s) {
         }
         else {
             if (verifyPair(s[k], stack[stack.length - 1])) {
+                stack.pop();
+            }
+            else {
+                stack.push(s[k]);
+            }
+        }
+    }
+
+    if (stack.length === 0)
+        return true
+
+    return false;
+};
+
+
+//More optimal solution - uses map to check pairs
+var isValid = function(s) {
+    //Faster mapping solution
+    let pairs = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+
+    let stack = [];
+    let k = s.length;
+    while (k--) {
+        if (stack.length === 0) {
+            stack.push(s[k]);
+        }
+        else {
+            if (pairs[s[k]] === stack[stack.length - 1]) {
                 stack.pop();
             }
             else {
