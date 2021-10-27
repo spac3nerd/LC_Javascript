a48bceebf0dce2670832a5cded3c354b67fa5417
spac3nerd <sorin.badila@yahoo.com>
2021-10-27 17:15:35 -0400

    added 1929

---
+++ 1929_ConcatenationOfArray/1929.js
@@ -0,0 +1,29 @@
+//This one is very trivial - not sure why they bothered
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+let getConcatenation1 = (nums) => {
+    let n = nums.length;
+    let c =  new Array(n);
+    nums.forEach((item, index) => {
+        c[index] = nums[index];
+        c[index + n] = nums[index];
+    });
+
+    return c;
+};
+
+let getConcatenation = (nums) => {
+    return [...nums,...nums];
+};
+
+
+
+runTest = () => {
+    console.log(getConcatenation([1,2,1]));
+    console.log(getConcatenation([1,3,2,1]));
+};
+
+runTest();
